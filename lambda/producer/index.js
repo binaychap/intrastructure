85b6b132ace716af3942ba3161813919fa49cdf3
Binay Rai <binaychap@gmail.com>
2026-04-26 15:43:27 -0400

    add producer to send message to sqs

--- lambda/producer/index.js
+++ lambda/producer/index.js
@@ -1,7 +1,29 @@
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+
+const sqsClient = new SQSClient({});
+
 exports.handler = async (event) => {
-  console.log("Producer event:", JSON.stringify(event));
+  const queueUrl = process.env.SQS_QUEUE_URL;
+
+  if (!queueUrl) {
+    throw new Error("SQS_QUEUE_URL is not set");
+  }
+
+  const messageBody = JSON.stringify({
+    source: "alb",
+    timestamp: new Date().toISOString(),
+    event,
+  });
+
+  await sqsClient.send(
+    new SendMessageCommand({
+      QueueUrl: queueUrl,
+      MessageBody: messageBody,
+    }),
+  );
+  console.log("Message sent to SQS:", messageBody);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Producer Lambda executed" }),
+    body: JSON.stringify({ message: "Message sent to SQS" }),
   };
 };
